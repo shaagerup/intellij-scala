dbe583093ce4fefb9c7894d7a7a7e45c12171b90
Mikhail Mutcianko <mutcianko.m@gmail.com>
2016-06-16 16:17:41 +0300

    fix inline keyword parsing

--- src/org/jetbrains/plugins/scala/lang/parser/ScalaPsiCreator.scala
+++ src/org/jetbrains/plugins/scala/lang/parser/ScalaPsiCreator.scala
@@ -6,6 +6,7 @@ import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiUtilCore
+import org.jetbrains.plugins.scala.lang.lexer.ScalaTokenTypes
 import org.jetbrains.plugins.scala.lang.psi.impl.base._
 import org.jetbrains.plugins.scala.lang.psi.impl.base.patterns._
 import org.jetbrains.plugins.scala.lang.psi.impl.base.types._
@@ -126,6 +127,7 @@ trait ScalaPsiCreator extends PsiCreator {
       /************** modifiers **************/
       case ScalaElementTypes.MODIFIERS => new ScModifierListImpl(node)
       case ScalaElementTypes.ACCESS_MODIFIER => new ScAccessModifierImpl(node)
+      case ScalaTokenTypes.kINLINE => new ScAccessModifierImpl(node)
 
       /************* annotation *************/
       case ScalaElementTypes.ANNOTATION => new ScAnnotationImpl(node)
